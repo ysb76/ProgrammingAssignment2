847f6aef80147af1418b379107be29a6c6f35830
bhcyas <meehanpm@comcast.net>
2014-10-26 17:52:15 -0400

    Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below will:
+##   makeCacheMatrix - create a matrix object that can cache its inverse
+##   cacheSolve - compute the inverse of the matrix returned by makeCacheMatrix or, 
+##   if the inverse matrix has already been cached, retrieves the inverse from the cache
 
-## Write a short comment describing this function
+## Create a matrix object and stores the inverse matrix
+makeCacheMatrix <- function(mat = matrix()) {
 
-makeCacheMatrix <- function(x = matrix()) {
-
+        #mat, ymat
+        #matinv, minv
+        
+        minv= NULL                 #use minv to store the matrix inverse
+        
+        set = function(ymat){
+                mat <<- ymat       #saves the input matrix into "mat" 
+                minv <<- NULL      #initiates the inverse matrix to be NULL
+        }
+        
+        get = function() mat      #returns the value of the original matrix
+        
+        setinvmat = function(matinv) minv <<- matinv  #stores the inverted matrix 
+        
+        getinvmat = function() minv   #After initial cacheSolve(), this returns the cached inverse matrix
+        
+        list(set=set, get=get, setinvmat=setinvmat, getinvmat=getinvmat)   
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Reads the matrix object created by makeCacheMatrix and returns the 
+## matrix inverse; if available, a cached inverse matrix will be returned.
+cacheSolve <- function(mat, ...) {
+        
+        minv = mat$getinvmat()
+        
+        if(!is.null(minv)){                             #check to see if minv has a non-null value
+                message("Getting cached data")
+                return(minv)                            #if so, return it
+        }
+        
+        data = mat$get()
+        
+        minv = solve(data, ...)                         #minv = invert mat
+        
+        mat$setinvmat(minv)
+        
+        minv
 }
